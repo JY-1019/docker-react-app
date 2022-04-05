73e8de34aa8dfb0c5248213121f2635c38dd0175
JY-1019 <yourgitemail@example.com>
2022-04-06 04:29:44 +0900

    commit1

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+// 두번 테스트 하는 것 실험
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
